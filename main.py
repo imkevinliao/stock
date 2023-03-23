71bea30f8c7806212415e2a2c4c7242e1650792a
imkevinliao <87458144+imkevinliao@users.noreply.github.com>
2023-03-23 11:42:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-from soc import Analyze, Download
+from soc import download
 
-# 个股成分信息 个股信息查询
-# data = ak.index_stock_hist()
-# data = ak.stock_individual_info_em()
-
-# inst = Download(fold=r"D:\github\save")
-
-import akshare as ak
-
-# data = ak.fund_etf_hist_em(symbol="510310")
-ak.fund_etf_fund_info_em()
-ak.fund_etf_hist_sina()
-
+codes = ["000300"]
+download(code=codes)
