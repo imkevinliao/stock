c91c8910e8d1ba0eb14fb5b4d99c2b73a0e62854
imkevinliao <87458144+imkevinliao@users.noreply.github.com>
2023-04-03 16:02:10 +0800

    Update demo.py

--- src/demo.py
+++ src/demo.py
@@ -1,12 +1,10 @@
-from soc import AnalyzeStock, AnalyzeFund
+from soc import Analyze, AnalyzeType
 
 # example
-codes = ["000001"]
-
-inst = AnalyzeFund(code=codes[0])
-inst.info()
-inst.query(compare_price=0.940, plot=True)
-
-inst = AnalyzeStock(code=codes[0])
+inst = Analyze(code="000001", analyze_type=AnalyzeType.STOCK)
 inst.info()
 inst.query(compare_price=12.82, plot=True)
+
+inst = Analyze(code="000001", analyze_type=AnalyzeType.FUND)
+inst.info()
+inst.query(compare_price=0.940, plot=True)
