0074e981a445307f4cc30697e9808cba5f9cbe72
imkevinliao <imkevinliao@gmail.com>
2023-04-13 15:02:51 +0800

    .

--- src/config.py
+++ src/config.py
@@ -16,5 +16,5 @@ current_day = time.strftime("%Y%m%d", time.localtime())
 current_hour = time.localtime().tm_hour
 current_min = time.localtime().tm_min
 
-json_source_path = join(basedir, "code")
+json_source_path = join(basedir, "resources")
 json_filepath = os.path.join(json_source_path, "code.json")
