bbf3a476da4486c53c198f31e6f520c3e8ad60be
imkevinliao <87458144+imkevinliao@users.noreply.github.com>
2023-04-13 14:43:08 +0800

    Update generate_json.py

--- src/generate_json.py
+++ src/generate_json.py
@@ -1,5 +1,6 @@
 import json
 import os.path
+import shutil
 
 import akshare as ak
 import pandas as pd
@@ -7,22 +8,50 @@ from easydict import EasyDict
 
 from config import *
 
+index_path = join(json_source_path, f"股票指数表.csv")
+fund_path = join(json_source_path, f"公募基金表.csv")
+stock_path = join(json_source_path, f"A股股票表.csv")
+
+
+def get_fund() -> dict:
+    if not os.path.exists(fund_path):
+        fund_name_em = ak.fund_name_em()
+        fund_name_em.to_csv(fund_path)
+    fund_dict = dict()
+    data = pd.read_csv(fund_path)
+    for index, row in data.iterrows():
+        key = str(row["基金代码"]).zfill(6)
+        value = str(row["基金简称"])
+        fund_dict[f"{key}"] = str(value)
+    return fund_dict
+
 
 def get_index() -> dict:
-    filepath = join(save_path_global, f"股票指数数据_{current_day}.csv")
-    if not os.path.exists(filepath):
-        if not os.path.exists(save_path_global):
-            os.mkdir(save_path_global)
+    if not os.path.exists(index_path):
         stock_zh_index_spot_df = ak.stock_zh_index_spot()
-        stock_zh_index_spot_df.to_csv(filepath)
+        stock_zh_index_spot_df.to_csv(index_path)
     
     index_dict = dict()
-    data = pd.read_csv(filepath)
+    data = pd.read_csv(index_path)
     for index, row in data.iterrows():
-        key = row["代码"]
-        value = row["名称"]
+        key = str(row["代码"])
+        value = str(row["名称"])
         index_dict[f"{key}"] = str(value)
     return index_dict
+
+
+def get_stock() -> dict:
+    if not os.path.exists(stock_path):
+        stock_info_a_code_name = ak.stock_info_a_code_name()
+        stock_info_a_code_name.to_csv(stock_path)
+    
+    stock_dict = dict()
+    data = pd.read_csv(stock_path)
+    for index, row in data.iterrows():
+        key = str(row["code"]).zfill(6)
+        value = str(row["name"])
+        stock_dict[f"{key}"] = str(value)
+    return stock_dict
 
 
 def gen_json(code_dict: dict):
@@ -31,7 +60,21 @@ def gen_json(code_dict: dict):
         f.write(json_str)
 
 
-if __name__ == '__main__':
+def core():
+    if not os.path.exists(json_source_path):
+        os.mkdir(json_source_path)
     code = EasyDict()
     code.index = get_index()
+    code.fund = get_fund()
+    code.stock = get_stock()
     gen_json(code)
+
+
+# 重新下载资源文件用于生成json
+def regenerate():
+    shutil.rmtree(json_source_path)
+    core()
+
+
+if __name__ == '__main__':
+    core()
